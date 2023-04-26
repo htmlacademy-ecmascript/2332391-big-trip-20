2871f07ded7c7fe23d6cc5e3f148727339cda792
denis_khmyrov <denis.e.khmyrov@gmail.com>
2023-04-26 22:05:22 +0300

    fix: console errors fixed

---
+++ src/main.js
@@ -0,0 +1,9 @@
+// import { render } from './render.js';
+// import { TripInfoTitleView } from './view/trip-info-title-view.js';
+
+// const tripMain = document.querySelector('.trip-main');
+// const tripInfoMain = tripMain.querySelector('trip-info__main');
+
+// render(new TripInfoTitleView, tripMain);
+
+console.log('hello!!!');
