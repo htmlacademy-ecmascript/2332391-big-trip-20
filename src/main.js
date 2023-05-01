5a6a340e0c284b8b5e7e5ed8788b34da56076689
denis_khmyrov <denis.e.khmyrov@gmail.com>
2023-05-01 11:37:02 +0300

    feat: all DOM-elements rendered using mentor method

--- src/main.js
+++ src/main.js
@@ -1,9 +1,32 @@
-// import { render } from './render.js';
-// import { TripInfoTitleView } from './view/trip-info-title-view.js';
+import './views/brief-view.js';
+import './views/add-button-view.js';
+import './views/filter-view.js';
+import './views/sort-view.js';
+import './views/list-view.js';
 
-// const tripMain = document.querySelector('.trip-main');
-// const tripInfoMain = tripMain.querySelector('trip-info__main');
+/**
+ * @type {BriefView}
+ */
+const briefView = document.querySelector('brief-view');
+/**
+ * @type {AddButtonView}
+ */
+const addButtonView = document.querySelector('add-button-view');
+/**
+ * @type {FilterView}
+ */
+const filterView = document.querySelector('filter-view');
+/**
+ * @type {SortView}
+ */
+const sortView = document.querySelector('sort-view');
+/**
+ * @type {ListView}
+ */
+const listView = document.querySelector('list-view');
 
-// render(new TripInfoTitleView, tripMain);
-
-console.log('hello!!!');
+briefView.render();
+addButtonView.render();
+filterView.render();
+sortView.render();
+listView.render();
