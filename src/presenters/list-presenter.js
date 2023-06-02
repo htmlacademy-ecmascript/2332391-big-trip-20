7359719abffab269c492325ea40b9ed17f5b86ae
Denis Khmyrov <denis.e.khmyrov@gmail.com>
2023-06-02 17:29:12 +0300

    Feat: +NewButton open/close function added

--- src/presenters/list-presenter.js
+++ src/presenters/list-presenter.js
@@ -18,6 +18,19 @@ class ListPresenter extends Presenter {
     const points = this.model.getPoints(urlParams);
     const items = points.map(this.createPointViewState, this);
 
+    if (urlParams.edit === 'draft') {
+      /**
+       *@type {Partial<Point>}
+       */
+      const draftPoint = {
+        type: 'taxi',
+        offerIds: [],
+        isFavorite: false
+      };
+
+      items.unshift(this.createPointViewState(draftPoint));
+    }
+    console.log(items);
     return {items};
   }
 
@@ -47,6 +60,8 @@ class ListPresenter extends Presenter {
      * @type {UrlParams}
      */
     const urlParams = this.getUrlParams();
+    const isDraft = point.id === undefined;
+    const isEditable = isDraft || point.id === urlParams.edit;
 
     return {
       id: point.id,
@@ -61,7 +76,8 @@ class ListPresenter extends Presenter {
       basePrice: point.basePrice,
       offers,
       isFavorite: point.isFavorite,
-      isEditable: point.id === urlParams.edit
+      isEditable,
+      isDraft,
     };
   }
 
--- src/views/editor-view.js
+++ src/views/editor-view.js
@@ -63,7 +63,8 @@ class EditorView extends View {
    * @param {SubmitEvent} event
    */
     handleReset(event) {
-      const actByDefault = this.notify('delete');
+      const point = this.state;
+      const actByDefault = this.notify(point.isDraft ? 'close' : 'delete');
 
       if(!actByDefault) {
         event.preventDefault();
@@ -215,6 +216,14 @@ class EditorView extends View {
    * @return {SafeHtml}
    */
   createResetButtonHtml() {
+    const point = this.state;
+
+    if (point.isDraft) {
+      return html`
+        <button class="event__reset-btn" type="reset">Cancel</button>
+      `;
+    }
+
     return html`
     <button class="event__reset-btn" type="reset">Delete</button>
     `;
@@ -224,6 +233,11 @@ class EditorView extends View {
    * @return {SafeHtml}
    */
   createCloseButtonHtml() {
+    const point = this.state;
+
+    if (point.isDraft) {
+      return '';
+    }
     return html`
       <button class="event__rollup-btn" type="button">
         <span class="visually-hidden">Close event</span>
