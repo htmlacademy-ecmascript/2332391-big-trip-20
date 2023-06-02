5064e72a23723446bc3f08a575b42b4287e7a18f
Denis Khmyrov <denis.e.khmyrov@gmail.com>
2023-06-02 17:04:44 +0300

    feat: +NewEvent button linked with url

--- src/presenters/add-button-presenter.js
+++ src/presenters/add-button-presenter.js
@@ -9,10 +9,36 @@ class AddButtonPresenter extends Presenter {
    * @return {AddViewState}
    */
   createViewState() {
+
+     /**
+     * @type {UrlParams}
+     */
+     const urlParams = this.getUrlParams();
     // TODO: create dynamically
     return {
-      isDisabled: false
+      isDisabled: urlParams.edit === 'draft'
     };
+  }
+
+  /**
+   * @override
+   */
+  addEventListeners() {
+    this.view.addEventListener('click', this.handleViewClick.bind(this));
+  }
+
+  /**
+   * @param {Event & {target: {value: FilterType}}} event
+   */
+  handleViewClick(event) {
+    /**
+     * @type {UrlParams}
+     */
+    const urlParams = {
+      edit: 'draft'
+    };
+    console.log(111);
+    this.setUrlParams(urlParams);
   }
 }
 
